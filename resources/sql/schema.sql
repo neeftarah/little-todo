d372f80a3b97d76c85ff67542a481adeb520da40
Jérémy Moreau <jmoreau@teclib.com>
2014-05-13 16:34:07 +0200

    Add first page

--- resources/sql/schema.sql
+++ resources/sql/schema.sql
@@ -1,5 +1,15 @@
-CREATE TABLE todo (
-    id          INTEGER PRIMARY KEY,
-    title       VARCHAR(255),
-    is_finished BOOLEAN
+CREATE TABLE projects (
+   id          INTEGER PRIMARY KEY,
+   name        VARCHAR(255),
+   orderno     INTEGER
+);
+CREATE TABLE tasks (
+   id          INTEGER PRIMARY KEY,
+   project_id  INTEGER,
+   title       VARCHAR(255),
+   orderno     INTEGER,
+   priority    INTEGER,
+   deadline    DATE,
+   is_finished BOOLEAN,
+   FOREIGN KEY(project_id) REFERENCES projects(id)
 );
