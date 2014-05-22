057b97a9ee668f68982104912afc8e963a023b5a
Jérémy Moreau <jmoreau@teclib.com>
2014-05-22 14:48:41 +0200

    Save data in DB

--- resources/sql/schema.sql
+++ resources/sql/schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE projects (
    id          INTEGER PRIMARY KEY,
    name        VARCHAR(255),
-   orderno     INTEGER
+   orderno     INTEGER DEFAULT 1
 );
 CREATE TABLE tasks (
    id          INTEGER PRIMARY KEY,
-   project_id  INTEGER,
+   project_id  INTEGER NOT NULL,
    title       VARCHAR(255),
-   orderno     INTEGER,
-   priority    INTEGER,
+   orderno     INTEGER DEFAULT 1,
+   priority    INTEGER DEFAULT 0,
    deadline    DATE,
-   is_finished BOOLEAN,
+   is_finished BOOLEAN NOT NULL DEFAULT 0,
    FOREIGN KEY(project_id) REFERENCES projects(id)
 );
--- src/app.php
+++ src/app.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 $app = new Silex\Application();
 
@@ -46,6 +47,7 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
     'monolog.name'    => 'littleTodo',
     'monolog.level'   => 300	// => Logger::WARNING
 ));
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 
 // Exception management
 $app->error(function(\Exception $e) use ($app) {
@@ -80,24 +82,93 @@ $app->get('/', function () use($app) {
    return $app['twig']->render('index.html.twig', array(
       'meta'            => array('title' => 'littleTodo'),
       'page'            => array('title' => 'littleTodo'),
-      'current_project' => 1,
-      'projects'        => array(
-                              1 => array('id' => 1, 'name' => 'Project 1', 'tasks' => '42'),
-                              2 => array('id' => 2, 'name' => 'Project 2', 'tasks' => '25'),
-                              3 => array('id' => 3, 'name' => 'Project 3', 'tasks' => '7'),
-                           ),
+      'current_project' => 0,
+      'projects'        => getProjects($app['pdo']),
+      'tasks'           => array(),
    ));
-});
+})
+->bind('homepage');
+
+$app->get('/project/{id}', function($id) use ($app) {
+   return $app['twig']->render('index.html.twig', array(
+      'meta'            => array('title' => 'littleTodo'),
+      'page'            => array('title' => 'littleTodo'),
+      'current_project' => $id,
+      'projects'        => getProjects($app['pdo']),
+      'tasks'           => getTasks($app['pdo'], $id),
+   ));
+})
+->bind('project');
 
 $app->post('/project/add', function (Request $request) use($app) {
    $project = $request->get('new_project');
-   $db = $app['pdo'];
+   $db      = $app['pdo'];
 
-   $st = $this->db->prepare("INSERT INTO projects (name) VALUES (:name)");
-   $st->bindValue(':name', $project);
+   try {
+      // Get order
+      $st = $db->prepare("SELECT MAX(orderno) FROM projects");
+      $st->execute();
+      list($orderno) = $st->fetch();
+      $orderno++;
+
+      $st = $db->prepare("INSERT INTO projects (name, orderno) VALUES (:name, :orderno)");
+      $st->bindValue(':name', $project);
+      $st->bindValue(':orderno', $orderno);
+      $st->execute();
+   } catch (Exception $e) {
+        return new Response('<h1>Insertion failed!</h1>', $e->getStatusCode());
+   }
+   return new Response('OK : ' . $project);
+
+   // $subRequest = Request::create($app['url_generator']->generate('homepage'), 'GET');
+   // return $app->handle($subRequest, HttpKernelInterface::SUB_REQUEST);
+   // return $app->redirect($app['url_generator']->generate('homepage'));
+})
+->bind('project_add');
+
+$app->post('/task/add', function (Request $request) use($app) {
+   $task       = $request->get('new_task');
+   $project_id = $request->get('current_project');
+   $db         = $app['pdo'];
+   try {
+      $st = $db->prepare("INSERT INTO tasks (project_id, title) VALUES (:project_id, :title)");
+      $st->bindValue(':project_id', $project_id);
+      $st->bindValue(':title', $task);
+      $st->execute();
+   } catch (Exception $e) {
+        return new Response('<h1>Insertion failed!</h1>', $e->getStatusCode());
+   }
+   return new Response('OK : ' . $task);
+
+   // $subRequest = Request::create($app['url_generator']->generate('homepage'), 'GET');
+   // return $app->handle($subRequest, HttpKernelInterface::SUB_REQUEST);
+   // return $app->redirect($app['url_generator']->generate('homepage'));
+})
+->bind('task_add');
+
+
+
+function getProjects($db) {
+   $query = "SELECT p.id, p.name, COUNT(t.id) AS tasks
+             FROM projects p
+             LEFT JOIN tasks t ON t.project_id = p.id
+             GROUP BY p.id
+             ORDER BY p.orderno";
+   $st = $db->prepare($query);
    $st->execute();
-
-   return $app->redirect('/');
-});
+   return $st->fetchAll();
+}
+function getTasks($db, $project_id, $finished = 0) {
+   $query = "SELECT id, title, deadline, priority
+             FROM tasks
+             WHERE project_id = :project_id
+             AND is_finished = :finished
+             ORDER BY deadline, priority";
+   $st = $db->prepare($query);
+   $st->bindValue(':project_id', $project_id);
+   $st->bindValue(':finished', $finished);
+   $st->execute();
+   return $st->fetchAll();
+}
 
 return $app;
